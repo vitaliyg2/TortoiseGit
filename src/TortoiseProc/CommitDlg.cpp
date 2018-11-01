vitaliyg2/TortoiseGit

commit 39a0d95ecc7ad8e0a63453cb48937b6f4403012b

Fixed issue #1131: "Whole project" checkbox in commit dialog selects all files
Signed-off-by: Sven Strickroth <email@cs-ware.de>

src/TortoiseProc/CommitDlg.cpp
@@ -1272,7 +1272,7 @@ void CCommitDlg::OnBnClickedShowunversioned()
 			else
 				m_ListCtrl.GetStatus(&this->m_pathList,false,false,true);
 		}
-		m_ListCtrl.Show(dwShow,0,true,0,true);
+		m_ListCtrl.Show(dwShow, 0, true, dwShow & ~(CTGitPath::LOGACTIONS_UNVER), true);
 	}
 }
 
@@ -2044,8 +2044,13 @@ void CCommitDlg::OnBnClickedWholeProject()
 		else
 			m_ListCtrl.GetStatus(&this->m_pathList,true,false,true);
 
-		DWORD dwShow = (DWORD)(GITSLC_SHOWVERSIONEDBUTNORMALANDEXTERNALSFROMDIFFERENTREPOS | GITSLC_SHOWUNVERSIONED | GITSLC_SHOWLOCKS);
-		m_ListCtrl.Show(m_ListCtrl.GetShowFlags(), dwShow & (~CTGitPath::LOGACTIONS_UNVER|~CTGitPath::LOGACTIONS_IGNORE));
+		DWORD dwShow = m_ListCtrl.GetShowFlags();
+		if (DWORD(m_regAddBeforeCommit))
+			dwShow |= GITSLC_SHOWUNVERSIONED;
+		else
+			dwShow &= ~GITSLC_SHOWUNVERSIONED;
+
+		m_ListCtrl.Show(dwShow, dwShow & ~(CTGitPath::LOGACTIONS_UNVER), true);
 	}
 
 	SetDlgTitle();

commit 5626b2edc31321b9e87979e5504691b6c4c8b595

Fixed incorrect parameter value introduced in commit 39a0d95ecc7ad8e0a63453cb48937b6f4403012b
Signed-off-by: Vitaliy Grabchuk <vitaliygrabchuk@gmail.com>

src/TortoiseProc/CommitDlg.cpp
@@ -1632,7 +1632,7 @@ void CCommitDlg::OnBnClickedShowunversioned()
 				m_ListCtrl.GetStatus(&this->m_pathList,false,false,true);
 		}
 		m_ListCtrl.StoreScrollPos();
-		m_ListCtrl.Show(dwShow, 0, true, dwShow & ~(CTGitPath::LOGACTIONS_UNVER), true);
+		m_ListCtrl.Show(dwShow, dwShow & ~(CTGitPath::LOGACTIONS_UNVER), true, FALSE, true);
 		UpdateCheckLinks();
 	}
 }